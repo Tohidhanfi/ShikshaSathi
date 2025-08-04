4a2f13a667953290584aa7c3d153d1f1805834f1
tohidhanfi <tohidhanfi@gmail.com>
2025-08-04 14:04:16 +0530

    ADDED galary to the webiste

--- src/js/script.js
+++ src/js/script.js
@@ -1145,4 +1145,24 @@ function trackPageView(pageName) {
     }
     
     localStorage.setItem('shikshaSathiAnalytics', JSON.stringify(analytics));
-} 
+}
+
+// Scroll to Top functionality
+const scrollToTopBtn = document.getElementById('scrollToTop');
+
+// Show/hide scroll to top button based on scroll position
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+        scrollToTopBtn.classList.add('show');
+    } else {
+        scrollToTopBtn.classList.remove('show');
+    }
+});
+
+// Scroll to top when button is clicked
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}); 
